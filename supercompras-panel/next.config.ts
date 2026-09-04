70f6b6681e681616169e8912e160585626956a6a
Benjamin <benjaarncivia@gmail.com>
2026-09-04 11:37:43 -0300

    arreglo

--- supercompras-panel/next.config.ts
+++ supercompras-panel/next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // Agregamos la IP de tu red para habilitar el acceso desde el teléfono y la tablet
   allowedDevOrigins: ["192.168.56.1", "192.168.1.117", "localhost"],
+  
+  // Agregamos esto para solucionar el error de PDFKit y sus fuentes en Vercel
+  experimental: {
+    serverComponentsExternalPackages: ["pdfkit"],
+  },
 };
 
 export default nextConfig;
